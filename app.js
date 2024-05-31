efd34942b247b26de7a064cc36f130763ed389eb
shagam <eli.shagam@gmail.com>
2024-06-01 01:40:09 +0300

    HTTPS WORKS	modified:   app.js

--- app.js
+++ app.js
@@ -1,11 +1,24 @@
 const express = require('express')
 const cors = require('cors')
+const https = require('https')
+const fs = require('fs')
 
 const app = express()
-const port = 3001
+const port = 5001
+var sslServer;
+
+sslServer = https.createServer({
+  key: fs.readFileSync('/etc/letsencrypt/live/dinagold.org/privkey.pem'),
+  cert: fs.readFileSync('/etc/letsencrypt/live/dinagold.org/fullchain.pem'),
+}, app)
+
+
 
 app.options('*', cors())
 app.use(cors())
+
+
+
 
 app.get('/ping', (req, res) => {
   console.log ('ping query', req.query)
@@ -13,7 +26,19 @@ app.get('/ping', (req, res) => {
   res.send('ok')
 })
 
-app.listen(port, () => {
+// sslServer.listen(port, (err) => {
+//   // console.log('secureServer on port=', port)
+//   if (err) {
+//     console.log('err: ', err)
+//   }
+// })
+
+
+sslServer.listen(port, (err) => {
+  if (err) {
+    console.log('err: ', err)
+  }
+
   console.log(`Example app listening on port ${port}`)
 })
 
